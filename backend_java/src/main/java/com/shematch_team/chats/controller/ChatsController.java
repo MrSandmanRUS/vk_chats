74e456a154dcd452236e4fec973a59f08e9078db
kruchon <krushon96@mail.ru>
2019-09-27 21:28:30 +0300

    get all chats rest paging

--- backend_java/src/main/java/com/shematch_team/chats/controller/ChatsController.java
+++ backend_java/src/main/java/com/shematch_team/chats/controller/ChatsController.java
@@ -3,10 +3,13 @@ package com.shematch_team.chats.controller;
 import com.shematch_team.chats.entity.Chat;
 import com.shematch_team.chats.repository.ChatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -20,8 +23,15 @@ public class ChatsController {
     }
 
     @GetMapping("getAllChats")
-    public ResponseEntity<List<Chat>> getAllChats(){
-        List<Chat> allChats = chatsRepository.findAll();
+    public ResponseEntity<List<Chat>> getAllChats(@RequestParam("start_id") Long startId,
+                                                  @RequestParam("page") Integer page) {
+        PageRequest pageable = new PageRequest(page, 20);
+        List<Chat> allChats;
+        if (startId == -1) {
+            allChats = chatsRepository.findAllByOrderByIdDesc(pageable);
+        } else {
+            allChats = chatsRepository.findAllByIdLessThanEqualOrderByIdDesc(startId, pageable);
+        }
         return ResponseEntity.ok(allChats);
     }
 
--- backend_java/src/main/java/com/shematch_team/chats/repository/ChatsRepository.java
+++ backend_java/src/main/java/com/shematch_team/chats/repository/ChatsRepository.java
@@ -1,8 +1,14 @@
 package com.shematch_team.chats.repository;
 
 import com.shematch_team.chats.entity.Chat;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
-public interface ChatsRepository extends JpaRepository<Chat, Long> {}
+public interface ChatsRepository extends JpaRepository<Chat, Long> {
+    ArrayList<Chat> findAllByOrderByIdDesc(Pageable pageable);
+    ArrayList<Chat> findAllByIdLessThanEqualOrderByIdDesc(Long id, Pageable pageable);
+}
