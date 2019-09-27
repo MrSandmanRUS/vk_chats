d90fa917f616de23a55947989489cc76dded5fdc
kruchon <krushon96@mail.ru>
2019-09-27 21:52:11 +0300

    get recommended from userchats

--- backend_java/src/main/java/com/shematch_team/chats/controller/ChatsController.java
+++ backend_java/src/main/java/com/shematch_team/chats/controller/ChatsController.java
@@ -2,25 +2,31 @@ package com.shematch_team.chats.controller;
 
 import com.shematch_team.chats.dto.UserRequestDto;
 import com.shematch_team.chats.entity.Chat;
+import com.shematch_team.chats.entity.User;
 import com.shematch_team.chats.repository.ChatsRepository;
+import com.shematch_team.chats.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class ChatsController {
 
     private final ChatsRepository chatsRepository;
+    private final UserRepository userRepository;
 
     @Autowired
-    public ChatsController(ChatsRepository chatsRepository) {
+    public ChatsController(ChatsRepository chatsRepository, UserRepository userRepository) {
         this.chatsRepository = chatsRepository;
+        this.userRepository = userRepository;
     }
 
     @GetMapping("getAll")
@@ -38,7 +44,10 @@ public class ChatsController {
 
     @PostMapping("getRecommended")
     public ResponseEntity<List<Chat>> getRecommended(UserRequestDto userRequestDto) {
-        return null;
+        String vkId = userRequestDto.getVkId();
+        User user = userRepository.findByVkId(vkId);
+        List<Chat> recommendedChats = new ArrayList<>(user.getChats());
+        return new ResponseEntity<>(recommendedChats, HttpStatus.OK);
     }
 
 
