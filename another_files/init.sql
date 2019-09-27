1af2b9730d48c9d3a8c6f6153402a0892f21f04d
kruchon <krushon96@mail.ru>
2019-09-27 18:56:00 +0300

    fixed create tables query

--- another_files/init.sql
+++ another_files/init.sql
@@ -3,19 +3,19 @@ create database chats;
 use chats;
 
 create table user (
-                      id bigint not null auto_increment,
+                      id bigint not null auto_increment primary key,
                       name varchar(128),
                       vk_id varchar(64),
                       info text
 );
 
 create table users_chats (
-                      id bigint not null auto_increment,
+                      id bigint not null auto_increment primary key,
                       user_id bigint not null,
                       chat_id bigint not null
 );
 
 create table chats (
-                       id bigint not null auto_increment,
+                       id bigint not null auto_increment primary key,
                        interest varchar(512)
 );
