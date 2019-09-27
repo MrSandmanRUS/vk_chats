7b415e6dd006fae0ce3035cc2c2496ea8c99e37f
Ilya Tkachev <skaatt@mail.ru>
2019-09-27 21:57:32 +0300

    restructered logic and added method predict

--- ml_backend/analyzer.py
+++ ml_backend/analyzer.py
@@ -1,30 +1,47 @@
 from pymorphy2 import MorphAnalyzer
 import nltk
-import json
 import re
 import heapq
 from collections import Counter
 
+with open('stopwords.txt', 'rt', encoding='utf-8') as file:
+    input_str = file.read()
+    stopwords = input_str.split(',')
 morphy_analyzer = MorphAnalyzer()
-####
-with open('../testapi/ml_backend/morf_vk.txt', 'r', encoding='utf-8-sig') as file:
-    txt = file.read()
-json_obj = json.loads(txt)
-input_txt = json_obj['groups']
-####
-text = ' '.join(input_txt)
-formatted_text = re.sub('[^a-zа-яА-ЯA-Zё]', ' ', text)
-formatted_text = re.sub(r'\b\w\b', ' ', formatted_text)
-formatted_text = re.sub(r'\s+', ' ', formatted_text)
-ru_stopwords = nltk.corpus.stopwords.words('russian')
-en_stopwords = nltk.corpus.stopwords.words('english')
 
-filtered_groups = filter(lambda x: _ if x in ru_stopwords or en_stopwords else x,
-                     nltk.word_tokenize(formatted_text.lower()))
-filtered_groups = filter(lambda x: len(x) > 3, filtered_groups)
-filtered_groups = (morphy_analyzer.parse(i)[0].normal_form for i in filtered_groups)
 
-frequent_words = Counter(filtered_groups)
+def most_chats(json_obj, numb_top):
+    input_txt = json_obj
+    text = ' '.join(input_txt)
+    formatted_text = re.sub('[^a-zа-яА-ЯA-Zё]', ' ', text)
+    formatted_text = re.sub(r'\b\w\b', ' ', formatted_text)
+    formatted_text = re.sub(r'\s+', ' ', formatted_text)
+    filtered_groups = filter(lambda x: x not in stopwords,
+                             nltk.word_tokenize(formatted_text.lower()))
+    filtered_groups = filter(lambda x: len(x) > 3, filtered_groups)
+    filtered_groups = (morphy_analyzer.parse(i)[0].normal_form for i in filtered_groups)
 
-topic_for_chat = heapq.nlargest(10, frequent_words, key=frequent_words.get)
-print(topic_for_chat)
+    frequent_words = Counter(filtered_groups)
+
+    topic_for_chat = heapq.nlargest(numb_top, frequent_words, key=frequent_words.get)
+
+    return topic_for_chat
+
+
+def generate_synonyms(top_charts, model, numb_syn):
+    generated_words = []
+    for word in top_charts:
+        try:
+            synoms = [i for i, j in model.most_similar(word, topn=numb_syn)]
+            generated_words.extend(synoms)
+        except KeyError:
+            continue
+    return generated_words
+
+
+def predict(model, user_groups, final_numb_topic=1, init_numb_top=3, numb_syn=10):
+    top_charts = most_chats(user_groups, init_numb_top)
+    list_gen_words = generate_synonyms(top_charts, model, numb_syn)
+    list_gen_words.extend(top_charts)
+    topic = most_chats(list_gen_words, final_numb_topic)
+    return topic
