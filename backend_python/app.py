ada7adb11cf8f36908c523b5465b18a49bdd2aa1
Виталий Монастырев <vitalijmonastyrev@MacBook-Pro-Vitalij.local>
2019-09-27 20:21:24 +0300

    Added ml API

--- backend_python/app.py
+++ backend_python/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def main():
     return ""
 
+@app.route("/getInterest")
+def getInterest():
+    return "some interest"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=81, use_reloader=False)
