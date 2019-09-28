b24772231b46c5c7719563980ad56284ed6da951
Anton Serdukov <serd.anton.777@gmail.com>
2019-09-28 11:11:04 +0400

    Добавлен макет для всех чатов

--- frontend/src/components/main/chats/all/index.js
+++ frontend/src/components/main/chats/all/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Panel, View} from "@vkontakte/vkui";
+import {Avatar, Cell, Group, List, Panel, PullToRefresh, Spinner, View} from "@vkontakte/vkui";
 
 const COMPONENT_NAME = 'ChatsAll';
 
@@ -14,8 +14,60 @@ class ChatsAll extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chats: []
+      chats: [],
+      fetching: false,
+      firstInit: true
     };
+    this.onRefresh = () => {this.updateChats();}
+  }
+
+  /**
+   * Вызывается при подключении компонента к дереву
+   */
+  componentDidMount() {
+    //  Шлем к бэку когда компонет подключился к дереву
+    this.updateChats();
+  }
+
+  /**
+   * Обновляет список чатов
+   */
+  updateChats() {
+    this.setState({
+      fetching: true
+    });
+
+    setTimeout(() => {
+      this.setState({
+        chats: [{id: 1, name: "Пиво, водка, пиво", photo: 'https://image.flaticon.com/icons/png/512/108/108331.png'}, ...this.state.chats],
+        fetching: false,
+        firstInit: false
+      });
+    }, 3000);
+  }
+
+  /**
+   * Отрисовывает спиннер старта
+   * @returns {*}
+   */
+  renderStartSpinner() {
+    if (this.state.firstInit) {
+      return (
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+          <Spinner size="large" style={{ marginTop: 20 }} />
+        </div>
+      )
+    }
+  }
+
+  /**
+   * Вызывает отрисовку чата
+   * @returns {*[]}
+   */
+  renderChats() {
+    return this.state.chats.map(({ id, name, photo }, i) =>
+      <Cell key={i} before={<Avatar src={photo} />}>Интересы: {name}</Cell>
+    );
   }
 
   /**
@@ -26,7 +78,17 @@ class ChatsAll extends React.Component {
     return (
       <View id={COMPONENT_NAME + 'View'} activePanel={COMPONENT_NAME + 'Panel'}>
         <Panel id={COMPONENT_NAME + 'Panel'}>
+          <Group>
+            { this.renderStartSpinner() }
 
+            <PullToRefresh onRefresh={this.onRefresh} isFetching={this.state.fetching}>
+              <Group>
+                <List>
+                  { this.renderChats() }
+                </List>
+              </Group>
+            </PullToRefresh>
+          </Group>
         </Panel>
       </View>
     );
