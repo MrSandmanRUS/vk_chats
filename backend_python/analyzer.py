a679f8253c569ed22d21e7593a0f60c4c3010c7a
Ilya Tkachev <skaatt@mail.ru>
2019-09-29 00:02:07 +0300

    added new fea

--- backend_python/analyzer.py
+++ backend_python/analyzer.py
@@ -4,6 +4,7 @@ import re
 import heapq
 from itertools import combinations
 from collections import Counter
+from .nltk_levenshtein import perform_corpus
 
 with open('stopwords.txt', 'rt', encoding='utf-8') as file:
     input_str = file.read()
@@ -19,10 +20,11 @@ def check_substring(a, b):
 
 
 def del_shortest_word(iter_obj):
-    return min(iter_obj, key=lambda x: len(x))
+    return min(iter_obj, key=len)
 
 
 def filter_verbs(word):
+    # del verbs
     if re.search('.*?[аиеёоуыэюя]ть(ся)?', word):
         if word.find('подслуш') != -1:
             return 'подслушано'
@@ -42,13 +44,14 @@ def most_chats(json_obj, numb_top, final_output):
     filtered_groups = (morphy_analyzer.parse(i)[0].normal_form for i in filtered_groups)
     filtered_groups = filter(lambda x: x not in stopwords, filtered_groups)
     filtered_groups = map(filter_verbs, filtered_groups)
+    # filtered_groups = list(filter(lambda x: len(x) > 1, map(filter_verbs, filtered_groups)))
+    # filtered_groups = perform_corpus(filtered_groups, 0.6)
     frequent_words = Counter(filtered_groups)
 
     if final_output:
         cognate_drop_words = list(map(del_shortest_word, filter(lambda x: check_substring(*x),
                                                                 combinations(frequent_words.keys(), 2))))
         frequent_words = dict((k, v) for k, v in frequent_words.items() if k not in cognate_drop_words)
-
     topic_for_chat = heapq.nlargest(numb_top, frequent_words, key=frequent_words.get)
     return topic_for_chat
 
