1e80c75eccbd90555bb3fb373f6bbd05b69b2da5
Ilya Tkachev <skaatt@mail.ru>
2019-09-28 18:14:17 +0300

    features: filter verbs, transform case with podslushano, filter cognate words

--- backend_python/analyzer.py
+++ backend_python/analyzer.py
@@ -2,6 +2,7 @@ from pymorphy2 import MorphAnalyzer
 import nltk
 import re
 import heapq
+from itertools import combinations
 from collections import Counter
 
 with open('stopwords.txt', 'rt', encoding='utf-8') as file:
@@ -10,20 +11,45 @@ with open('stopwords.txt', 'rt', encoding='utf-8') as file:
 morphy_analyzer = MorphAnalyzer()
 
 
-def most_chats(json_obj, numb_top):
+def check_substring(a, b):
+    if b.find(a) != -1 or a.find(b) != -1:
+        return True
+    else:
+        return False
+
+
+def del_shortest_word(iter_obj):
+    return min(iter_obj, key=lambda x: len(x))
+
+
+def filter_verbs(word):
+    if re.search('.*?[аиеёоуыэюя]ть(ся)?', word):
+        if word.find('подслуш') != -1:
+            return 'подслушано'
+        return ''
+    else:
+        return word
+
+
+def most_chats(json_obj, numb_top, final_output):
     text = ' '.join(json_obj)
     formatted_text = re.sub('[^a-zа-яА-ЯA-Zё]', ' ', text)
     formatted_text = re.sub(r'\b\w\b', ' ', formatted_text)
     formatted_text = re.sub(r'\s+', ' ', formatted_text)
     filtered_groups = filter(lambda x: x not in stopwords,
                              nltk.word_tokenize(formatted_text.lower()))
-    filtered_groups = filter(lambda x: len(x) > 3, filtered_groups)
+    filtered_groups = filter(lambda x: len(x) > 2, filtered_groups)
     filtered_groups = (morphy_analyzer.parse(i)[0].normal_form for i in filtered_groups)
-
+    filtered_groups = filter(lambda x: x not in stopwords, filtered_groups)
+    filtered_groups = map(filter_verbs, filtered_groups)
     frequent_words = Counter(filtered_groups)
 
-    topic_for_chat = heapq.nlargest(numb_top, frequent_words, key=frequent_words.get)
+    if final_output:
+        cognate_drop_words = list(map(del_shortest_word, filter(lambda x: check_substring(*x),
+                                                                combinations(frequent_words.keys(), 2))))
+        frequent_words = dict((k, v) for k, v in frequent_words.items() if k not in cognate_drop_words)
 
+    topic_for_chat = heapq.nlargest(numb_top, frequent_words, key=frequent_words.get)
     return topic_for_chat
 
 
@@ -39,10 +65,11 @@ def generate_synonyms(top_charts, model, numb_syn):
 
 def predict(model, user_groups, final_numb_topic=5, init_numb_top=5, numb_syn=10):
     if user_groups:
-        top_charts = most_chats(user_groups, init_numb_top)
+        top_charts = most_chats(user_groups, init_numb_top, False)
         list_gen_words = generate_synonyms(top_charts, model, numb_syn)
         list_gen_words.extend(top_charts)
-        topics = most_chats(list_gen_words, final_numb_topic)
+        topics = most_chats(list_gen_words, final_numb_topic, True)
     else:
         topics = ['аноним']
+
     return topics
