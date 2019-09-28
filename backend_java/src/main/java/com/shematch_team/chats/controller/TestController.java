96e191ec5385424a1138766f5bc1a863c3d9b25c
Виталий Монастырев <vitalijmonastyrev@MacBook-Pro-Vitalij.local>
2019-09-28 08:37:08 +0300

    Added ml api support

--- backend_java/src/main/java/com/shematch_team/chats/controller/TestController.java
+++ backend_java/src/main/java/com/shematch_team/chats/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.shematch_team.chats.controller;
 
+import com.shematch_team.chats.component.InterestGen;
 import com.shematch_team.chats.component.PhotoSearch;
+import com.shematch_team.chats.dto.UserRequestDto;
 import com.shematch_team.chats.repository.ChatsRepository;
 import com.shematch_team.chats.repository.UserRepository;
 import com.shematch_team.chats.repository.UsersChatsRepository;
@@ -8,6 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class TestController {
@@ -16,15 +23,19 @@ public class TestController {
     private final UserRepository userRepository;
     private final UsersChatsRepository usersChatsRepository;
     private final PhotoSearch photoSearch;
+    private final InterestGen interestGen;
 
     @Autowired
     public TestController(ChatsRepository chatsRepository,
                           UserRepository userRepository,
-                          UsersChatsRepository usersChatsRepository, PhotoSearch photoSearch) {
+                          UsersChatsRepository usersChatsRepository,
+                          PhotoSearch photoSearch,
+                          InterestGen interestGen) {
         this.chatsRepository = chatsRepository;
         this.userRepository = userRepository;
         this.usersChatsRepository = usersChatsRepository;
         this.photoSearch = photoSearch;
+        this.interestGen = interestGen;
     }
 
     @GetMapping("test")
@@ -38,6 +49,14 @@ public class TestController {
 
 
         return ResponseEntity.ok(page);
+    }
+
+    @PostMapping("getInterest")
+    public ResponseEntity<String> getInterest(@RequestBody UserRequestDto userRequestDto) throws IOException {
+        List<String> interest = interestGen.getInterestsFromPython(userRequestDto);
+
+
+        return ResponseEntity.ok(interest.toString());
     }
 
 }
